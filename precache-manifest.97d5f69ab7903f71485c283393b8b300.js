9184c8e0cffca089e6e201cc8bfe09dc5451a43b
compinschools <colinsmith@smithcolin.co.uk>
2022-06-23 09:44:57 +0100

    Updates

---
+++ precache-manifest.97d5f69ab7903f71485c283393b8b300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aaeeeaf7745a12564cf6de1c6b64434",
+    "url": "/MyReactGHP/index.html"
+  },
+  {
+    "revision": "da59e40823dec7a02248",
+    "url": "/MyReactGHP/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f3fafc787783f2ef54cd",
+    "url": "/MyReactGHP/static/js/2.4294d90b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyReactGHP/static/js/2.4294d90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da59e40823dec7a02248",
+    "url": "/MyReactGHP/static/js/main.d406349f.chunk.js"
+  },
+  {
+    "revision": "083c61ff0bf2d884e336",
+    "url": "/MyReactGHP/static/js/runtime-main.31d535c6.js"
+  }
+]);
